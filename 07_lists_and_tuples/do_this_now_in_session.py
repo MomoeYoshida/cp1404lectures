098b7f4f00dbb940319ed00dbf6f30e88e5eece1
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-15 08:57:30 +0900

    Complete week 4 get-age function demo

--- 07_lists_and_tuples/do_this_now_in_session.py
+++ 07_lists_and_tuples/do_this_now_in_session.py
@@ -9,10 +9,10 @@ MAXIMUM_AGE = 150
 
 def main():
     """Get and print user's age using functions."""
-    age = int(input("Enter your age (0-150): "))
+    age = int(input("Enter your age (0-150): "))  # Refactor > Extract Method from here
     while not MINIMUM_AGE <= age <= MAXIMUM_AGE:
         print("Invalid age!")
-        age = int(input("Enter your age (0-150): "))
+        age = int(input("Enter your age (0-150): "))  # until this line!
     print("Your age is {}.".format(age))
 
 
