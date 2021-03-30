34aa78f5b9240f86264848ad3c6021c4be329993
MomoeYoshida <momoe.yoshida@my.jcu.edu.au>
2021-03-30 14:06:36 +0900

    Add

--- classes/student.py
+++ classes/student.py
@@ -21,6 +21,7 @@ first_name = input("First name: ")
 last_name = input("Last name: ")
 student_id = int(input("ID: "))
 s1 = Student(first_name, last_name, student_id)
+print(s1)
 print(s1.first_name, "has ID", s1.id)
 
 if __name__ == '__main__':
